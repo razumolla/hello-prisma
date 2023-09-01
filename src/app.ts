8cefac269e1f7ca3a7fa1ea8e9c71b091500dcfc
razumolla <rajuahmed9344@gmail.com>
2023-09-01 21:48:40 +0600

    create category

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import { UserRouter } from "./modules/user/user.route";
+import { CategoryRoutes } from "./modules/category/category.route";
 
 const app: Application = express();
 app.use(express.json());
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/category", CategoryRoutes);
+// app.use("/api/v1/post", PostRoutes)
 
 export default app;
