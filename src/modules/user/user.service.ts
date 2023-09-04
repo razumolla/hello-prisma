32851d15cebf186b5c574394f63500c831cd6d41
razumolla <rajuahmed9344@gmail.com>
2023-09-04 11:14:33 +0600

    row query

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -34,15 +34,17 @@ const insertOrUpdateProfile = async (data: Profile): Promise<Profile> => {
 };
 
 const getUsers = async () => {
-  const result = await prisma.user.findMany({
-    // select: {
-    //   email: true,
-    // },
+  // const result = await prisma.user.findMany({
+  //   // select: {
+  //   //   email: true,
+  //   // },
 
-    include: {
-      profile: true,
-    },
-  });
+  //   include: {
+  //     profile: true,
+  //   },
+  // });
+  const result = await prisma.$queryRaw` select * from users `;
+
   return result;
 };
 
